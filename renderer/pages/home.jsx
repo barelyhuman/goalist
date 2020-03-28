26451c6a1444afd065b27aae0b56027989065db2
Siddharth Gelera <sidgroks@gmail.com>
2020-03-29 03:32:42 +0530

    base app

---
+++ renderer/pages/home.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+  CSSBaseline,
+  ZEITUIProvider,
+  Text,
+  Row
+} from '@zeit-ui/react'
+
+import GoalList from '../components/GoalList';
+
+import Head from 'next/head';
+import Link from 'next/link';
+
+const Next = () => {
+  return (
+    <ZEITUIProvider>
+      <CSSBaseline />
+      <Head>
+        <title>Goalist</title>
+      </Head>
+      <div className="p-lg">
+        <Row justify="center" align="center">
+          <Text h2>Goalist</Text>
+        </Row>
+        <Row justify="center" align="center" style={{ marginBottom: '15px' }}>
+        <Text small>Made with &hearts; at BarelyHuman</Text>
+        </Row>
+        <div>
+          <GoalList />
+        </div>
+        <style jsx global>
+          {`
+
+          *{
+            box-sizing:border-box
+          }
+
+          .p-sm{
+            padding:5px
+          }
+
+          .p-md{
+            padding:10px
+          }
+
+          .p-lg{
+            padding:15px
+          }
+
+        `}
+        </style>
+      </div>
+    </ZEITUIProvider>
+  );
+};
+
+export default Next;
